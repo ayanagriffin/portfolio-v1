2651a4c1d462e33ac5add8a7dfeba04ceb409002
Ayana Griffin <ayanajg@gmail.com>
2020-10-16 17:06:58 -0700

    tooltip for dark mode icon

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,3 +1,37 @@
+ // TAKE THE USER TO THE ABOUT ME PAGE IF THEY NAVIGATE TO IT ON THE NAVBAR
+ const aboutBtn = document.querySelector("#about");
+ aboutBtn.addEventListener('click', () => {
+     localStorage.setItem('about-me', 'true');
+
+ });
+
+ //CHECK THE ORIGINAL COLOR SETTING TO DETERMINE WHICH ICON SHOULD BE DISPLAYED
+ if ($('body').hasClass('dark')) {
+     $("#darkmode").html('<i id="darkmode" class="fas fa-cloud-sun"></i>');
+     $("#darkmode-tip").html('switch to light mode')
+ } else {
+     $("#darkmode").html('<i id="darkmode" class="fas fa-cloud-moon"></i>');
+     $("#darkmode-tip").html('switch to dark mode')
+ }
+ //SWITCH TO DARK MODE BY ADDING CLASS 'DARK' TO BODY WHEN BTN IS CLICKED
+ const dark_btn = document.querySelector('#darkmode');
+ const body = document.querySelector('body');
+
+
+ dark_btn.addEventListener('click', () => {
+     $('body').toggleClass('dark');
+     if ($('body').hasClass('dark')) {
+         localStorage.setItem("darkmode", "true");
+         $("#darkmode").html('<i id="darkmode" class="fas fa-cloud-sun"></i>');
+         $("#darkmode-tip").html('switch to light mode')
+     } else {
+         localStorage.setItem("darkmode", "false");
+         $("#darkmode").html('<i id="darkmode" class="fas fa-cloud-moon"></i>');
+         $("#darkmode-tip").html('switch to dark mode')
+     }
+
+ })
+
 // CLOSES RESPONSIVE MENU WHEN USER CLICKS LINK
 
 
@@ -12,6 +46,7 @@ $(document).ready(function () {
         })
 
     });
+
 
    
 });
