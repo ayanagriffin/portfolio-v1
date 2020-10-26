bf0f490b60f1e16f3af7895acc30a533420c18e4
Ayana Griffin <ayanajg@gmail.com>
2020-10-26 14:52:00 -0700

    cookies

--- javascript/project-selector.js
+++ javascript/project-selector.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     // SELECTING PROJECT
 
-    $('.learnMore').click(function(){
-        const value =  $(this).attr('id');
-        sessionStorage.setItem('project', value);
+    // $('.learnMore').click(function(){
+    //     const value =  $(this).attr('id');
+    //     sessionStorage.setItem('project', value);
 
-    })
+    // })
 
 
 
