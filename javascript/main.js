3b0a96530bba1666422ca258860d519c98de6e7c
Ayana Griffin <ayanajg@gmail.com>
2021-01-14 15:25:12 -0800

    new footer

--- javascript/main.js
+++ javascript/main.js
@@ -124,3 +124,33 @@ send_btn.addEventListener('click', () => {
 });
 
 // });
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+console.log(document.body.scrollHeight);
+function scrollFunction() {
+    if (window.pageYOffset > (document.body.scrollHeight / 1.5)) { // Show backToTopButton
+        if (!backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnExit");
+            backToTopButton.classList.add("btnEntrance");
+            backToTopButton.style.display = "block";
+        }
+    }
+    else { // Hide backToTopButton
+        if (backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnEntrance");
+            backToTopButton.classList.add("btnExit");
+            setTimeout(function () {
+                backToTopButton.style.display = "none";
+            }, 250);
+        }
+    }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    window.scrollTo(0, 0);
+}
